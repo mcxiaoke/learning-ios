e66ce946d470072d06881d6b2369820bc9aed3c5
mcxiaoke <mcxiaoke@gmail.com>
2015-08-02 21:44:50 +0800

    add empty FoundationEx project

---
+++ FoundationEx/FoundationEx/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FoundationEx
+//
+//  Created by mcxiaoke on 15/8/2.
+//  Copyright (c) 2015年 mcxiaoke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

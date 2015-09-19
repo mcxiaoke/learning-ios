faa6b287b4b790b06685d1878a13b42d7e77b3be
mcxiaoke <mcxiaoke@gmail.com>
2015-09-19 22:07:16 +0800

    add image assets for bullseye

--- ios_apprentice_2/BullsEye/BullsEye/AboutViewController.m
+++ ios_apprentice_2/BullsEye/BullsEye/AboutViewController.m
@@ -9,33 +9,25 @@
 #import "AboutViewController.h"
 
 @interface AboutViewController ()
+@property(weak, nonatomic) IBOutlet UIWebView* webview;
 
 @end
 
 @implementation AboutViewController
-- (IBAction)onBackPressed:(UIButton *)sender {
+- (IBAction)onBackPressed:(UIButton*)sender {
   [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view.
+  NSString* htmlFile =
+      [[NSBundle mainBundle] pathForResource:@"BullsEye" ofType:@"html"];
+  NSData* htmlData = [NSData dataWithContentsOfFile:htmlFile];
+  NSURL* baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];
+  [self.webview loadData:htmlData
+                MIMEType:@"text/html"
+        textEncodingName:@"UTF-8"
+                 baseURL:baseURL];
 }
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little
-preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

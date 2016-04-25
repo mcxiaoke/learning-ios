669bde3f3601ba0dc743663f6f182338bf6f2313
mcxiaoke <mcxiaoke@gmail.com>
2016-04-25 14:36:07 +0800

    tweak otp demo

---
+++ osx_learning_demos/OTPDemo/OTPDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OTPDemo
+//
+//  Created by mcxiaoke on 15/11/12.
+//  Copyright © 2015年 mcxiaoke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+

389965077f963d2871a05c051c1a70379ff6ca67
mcxiaoke <mcxiaoke@gmail.com>
2016-04-28 11:43:25 +0800

    edit row for simple todo

--- cocoa_programming_for_osx/07_TableViews/SimpleToDo/SimpleToDo/MainWindowController.swift
+++ cocoa_programming_for_osx/07_TableViews/SimpleToDo/SimpleToDo/MainWindowController.swift
@@ -14,13 +14,19 @@ class MainWindowController: NSWindowController,
         NSTextFieldDelegate{
   
   var items:[String] = []
+  var curIndex = NSNotFound
   
   @IBOutlet weak var textField: NSTextField!
   @IBOutlet weak var tableView: NSTableView!
   @IBOutlet weak var addButton: NSButton!
   
   @IBAction func addItem(sender: AnyObject) {
-    self.items.append(self.textField.stringValue)
+    if curIndex != NSNotFound {
+      self.items[curIndex] = self.textField.stringValue
+    }else {
+      self.items.append(self.textField.stringValue)
+    }
+    self.curIndex = NSNotFound
     self.textField.stringValue = ""
     updateUI()
     self.tableView.reloadData()
@@ -34,6 +40,10 @@ class MainWindowController: NSWindowController,
     self.addButton.enabled = !self.textField.stringValue.isEmpty
   }
   
+  func deleteRow(action:NSTableViewRowAction, row:Int) {
+    //
+  }
+  
   // table view
   func numberOfRowsInTableView(tableView: NSTableView) -> Int {
     return items.count
@@ -43,6 +53,21 @@ class MainWindowController: NSWindowController,
     return items[row]
   }
   
+  
+  func tableView(tableView: NSTableView, rowActionsForRow row: Int, edge: NSTableRowActionEdge) -> [NSTableViewRowAction] {
+    let deleteAction = NSTableViewRowAction(style: NSTableViewRowActionStyle.Destructive, title: "Delete", handler: { action, row in
+          print("action = \(action) row = \(row)")
+    })
+    return [deleteAction]
+  }
+  
+  
+  func tableViewSelectionDidChange(notification: NSNotification) {
+    let row = self.tableView.selectedRow
+    self.curIndex = row
+    self.textField.stringValue = items[row]
+  }
+  
   override var windowNibName: String? {
     return "MainWindowController"
   }
@@ -50,8 +75,9 @@ class MainWindowController: NSWindowController,
     override func windowDidLoad() {
         super.windowDidLoad()
       self.textField.delegate = self
-      items.append("First Item")
-      items.append("Second Item")
+      for i in 1...10 {
+        items.append("Simple ToDo List Item No.\(i)")
+      }
       updateUI()
     }
     

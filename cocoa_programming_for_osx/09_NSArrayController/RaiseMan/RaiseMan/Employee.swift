b68c8f3524b6c6d2539b014641f26e3b8a3ec9fc
mcxiaoke <mcxiaoke@gmail.com>
2016-05-03 17:30:20 +0800

    update raise man for ch10

--- cocoa_programming_for_osx/09_NSArrayController/RaiseMan/RaiseMan/Employee.swift
+++ cocoa_programming_for_osx/09_NSArrayController/RaiseMan/RaiseMan/Employee.swift
@@ -11,4 +11,20 @@ import Foundation
 class Employee: NSObject {
   var name: String? = "New Employee"
   var raise: Float = 0.05
+  
+  func validateRaise(raiseNumbeerPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>,
+                     error outError: NSErrorPointer) -> Bool {
+    let raiseNumber = raiseNumbeerPointer.memory
+    if raiseNumber == nil {
+      let domain = "UserInputValidationErrorDomain"
+      let code = 0
+      let userInfo = [NSLocalizedDescriptionKey: "An employee's raise must be a number"]
+      outError.memory = NSError(domain: domain, code: code, userInfo: userInfo)
+      return false
+//      raiseNumbeerPointer.memory = 0.0
+//      return true
+    }else{
+      return true
+    }
+  }
 }

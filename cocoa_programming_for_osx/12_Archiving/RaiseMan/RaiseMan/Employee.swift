db31dc69cf3dbf36a5b983b02c9fbaab1c9ace75
mcxiaoke <mcxiaoke@gmail.com>
2016-05-04 15:03:01 +0800

    update raise man app, add nscoding

---
+++ cocoa_programming_for_osx/12_Archiving/RaiseMan/RaiseMan/Employee.swift
@@ -0,0 +1,52 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by mcxiaoke on 16/4/29.
+//  Copyright © 2016年 mcxiaoke. All rights reserved.
+//
+
+import Foundation
+
+class Employee: NSObject, NSCoding {
+  var name: String? = "New Employee"
+  var raise: Float = 0.05
+  
+  override var description: String {
+    return "Employee(\(name!), \(raise))"
+  }
+  
+  func validateRaise(raiseNumbeerPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>,
+                     error outError: NSErrorPointer) -> Bool {
+    let raiseNumber = raiseNumbeerPointer.memory
+    if raiseNumber == nil {
+      let domain = "UserInputValidationErrorDomain"
+      let code = 0
+      let userInfo = [NSLocalizedDescriptionKey: "An employee's raise must be a number"]
+      outError.memory = NSError(domain: domain, code: code, userInfo: userInfo)
+      return false
+//      raiseNumbeerPointer.memory = 0.0
+//      return true
+    }else{
+      return true
+    }
+  }
+  
+  override init() {
+    super.init()
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    name = aDecoder.decodeObjectForKey("name") as! String?
+    raise = aDecoder.decodeFloatForKey("raise")
+    super.init()
+    // super.init(coder: aDecoder)
+  }
+  
+  func encodeWithCoder(aCoder: NSCoder) {
+    if let name = name {
+      aCoder.encodeObject(name, forKey: "name")
+    }
+    aCoder.encodeFloat(raise, forKey: "raise")
+  }
+}

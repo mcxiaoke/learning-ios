e90401177a218973ab6d0503359afddc1d12009a
mcxiaoke <mcxiaoke@gmail.com>
2016-05-27 11:52:41 +0800

    exif viewer, async load properties

--- osx_learning_demos/ExifViewer/ExifViewer/ImageDetailViewController.swift
+++ osx_learning_demos/ExifViewer/ExifViewer/ImageDetailViewController.swift
@@ -8,6 +8,27 @@
 
 import Cocoa
 
+class ExifInfo {
+  let key:String
+  let value:AnyObject?
+  
+  init(key:String, value:AnyObject?){
+    self.key = key
+    self.value = value
+  }
+}
+
+class ImageInfo {
+  let fileName:String
+  let width:Int = 0
+  let height:Int = 0
+  var properties:[[String:ExifInfo]] = []
+  
+  init(fileName: String){
+    self.fileName = fileName
+  }
+}
+
 class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
   
   var imageURL:NSURL? {
@@ -47,15 +68,6 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
         self.image = image
       })
     }
-  }
-  
-  func loadImageProperties(url:NSURL){
-    self.parseExifInfo(url)
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    print("viewDidLoad")
   }
   
   func thumbImageFromImage(url: NSURL) -> NSImage {
@@ -106,6 +118,62 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
     return array
   }
   
+  func loadImageProperties(url: NSURL){
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+      if let imageSource = CGImageSourceCreateWithURL(url, nil) {
+        if let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?{
+          let width = imageProperties[kCGImagePropertyPixelWidth] as! Int
+          let height = imageProperties[kCGImagePropertyPixelHeight] as! Int
+          print("Image width=\(width) height=\(height)")
+          
+          let properties = self.parseToTree(imageProperties)
+          dispatch_async(dispatch_get_main_queue(), {
+            self.properties = properties
+            self.outlineView.expandItem(nil, expandChildren: true)
+            //      let exif = imageProperties[kCGImagePropertyExifDictionary as String]
+            //      let gps = imageProperties[kCGImagePropertyGPSDictionary as String]
+            //      let tiff = imageProperties[kCGImagePropertyTIFFDictionary as String]
+          })
+        }
+      }
+    }
+  }
+  
+  func writeProperties(url:NSURL){
+    let imageSource = CGImageSourceCreateWithURL(url, nil)
+    if imageSource == nil {
+      return
+    }
+    let uti = CGImageSourceGetType(imageSource!)!
+    let data = NSMutableData()
+    let imageDestination = CGImageDestinationCreateWithData(data, uti, 1, nil)
+    
+    if imageDestination == nil {
+      return
+    }
+    
+    let gpsDict = [
+      kCGImagePropertyGPSDateStamp as String : "2016:05:08",
+      kCGImagePropertyGPSTimeStamp as String : "05:44:00",
+      kCGImagePropertyGPSLongitudeRef as String : "E",
+      kCGImagePropertyGPSLongitude as String : 108.389555,
+      kCGImagePropertyGPSLatitudeRef as String : "N",
+      kCGImagePropertyGPSLatitude as String : 22.785911666
+    ]
+    let metaDict = [kCGImagePropertyGPSDictionary as String : gpsDict]
+    
+    CGImageDestinationAddImageFromSource(imageDestination!, imageSource!, 0, metaDict)
+    CGImageDestinationFinalize(imageDestination!)
+      
+//      let directory = NSSearchPathForDirectoriesInDomains(.DownloadsDirectory, .UserDomainMask, true).first!
+//      let dateFormatter = NSDateFormatter()
+//      dateFormatter.dateFormat="yyyyMMdd_HHmmss"
+//      let newFileName = "\(dateFormatter.stringFromDate(NSDate())).jpg"
+//      let writePath = NSURL(fileURLWithPath:directory).URLByAppendingPathComponent(newFileName)
+      //        print("Image \(url) saved to \(writePath)")
+      //        _ = try? data.writeToURL(writePath, options: NSDataWritingOptions.AtomicWrite)
+  }
+  
   // https://github.com/oopww1992/WWimageExif
   // http://oleb.net/blog/2011/09/accessing-image-properties-without-loading-the-image-into-memory/
   // https://developer.apple.com/library/ios/documentation/GraphicsImaging/Reference/CGImageProperties_Reference/index.html
@@ -114,70 +182,6 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
   // CFDictionary can cast to Dictionary?
   // CFString can cast to String
   // http://stackoverflow.com/questions/32716146/cfdictionary-wont-bridge-to-nsdictionary-swift-2-0-ios9
-  func parseExifInfo(url: NSURL){
-    let imageSource = CGImageSourceCreateWithURL(url, nil)
-    if imageSource == nil {
-      return
-    }
-//    let imageOptions:[String:Bool] = [kCGImageSourceShouldCache as String: false]
-//    if let imageTags = CGImageSourceCopyMetadataAtIndex(imageSource, 0, nil){
-//      CGImageMetadataEnumerateTagsUsingBlock(imageTags, nil , nil, { (path, tag) in
-//        let tagName = CGImageMetadataTagCopyName(tag)!
-//        let tagValue = CGImageMetadataTagCopyValue(tag)!
-//        print("[\(path)] [\(tagName)=\(tagValue)]")
-//        return true
-//      })
-//    }
-    if let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource!, 0, nil) as Dictionary?{
-      self.properties = parseToTree(imageProperties)
-      self.outlineView.expandItem(nil, expandChildren: true)
-      
-//      let exif = imageProperties[kCGImagePropertyExifDictionary as String]
-      let gps = imageProperties[kCGImagePropertyGPSDictionary as String]
-//      let tiff = imageProperties[kCGImagePropertyTIFFDictionary as String]
-      
-      if gps == nil {
-        print("No GPS Data!")
-        let uti = CGImageSourceGetType(imageSource!)!
-        let data = NSMutableData()
-        let imageDestination = CGImageDestinationCreateWithData(data, uti, 1, nil)
-        
-        if imageDestination == nil {
-          return
-        }
-        
-        let gpsDict = [
-          kCGImagePropertyGPSDateStamp as String : "2016:05:08",
-          kCGImagePropertyGPSTimeStamp as String : "05:44:00",
-          kCGImagePropertyGPSLongitudeRef as String : "E",
-          kCGImagePropertyGPSLongitude as String : 108.389555,
-          kCGImagePropertyGPSLatitudeRef as String : "N",
-          kCGImagePropertyGPSLatitude as String : 22.785911666
-        ]
-        let metaDict = [kCGImagePropertyGPSDictionary as String : gpsDict]
-        
-        CGImageDestinationAddImageFromSource(imageDestination!, imageSource!, 0, metaDict)
-        CGImageDestinationFinalize(imageDestination!)
-        
-        let directory = NSSearchPathForDirectoriesInDomains(.DownloadsDirectory, .UserDomainMask, true).first!
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat="yyyyMMdd_HHmmss"
-        let newFileName = "\(dateFormatter.stringFromDate(NSDate())).jpg"
-        let writePath = NSURL(fileURLWithPath:directory).URLByAppendingPathComponent(newFileName)
-//        print("Image \(url) saved to \(writePath)")
-//        _ = try? data.writeToURL(writePath, options: NSDataWritingOptions.AtomicWrite)
-      }
-      
-//      if let exif = exif as? Dictionary<String, AnyObject> {
-//      }
-//      if let gps = gps as? Dictionary<String, AnyObject> {
-//      }
-//      if let tiff = tiff as? Dictionary<String, AnyObject> {
-//      }
-//      self.tableView?.reloadData()
-    }
-    
-  }
 
 
 }

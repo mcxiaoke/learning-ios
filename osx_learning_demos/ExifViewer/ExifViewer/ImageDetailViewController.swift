59cc73df42f51aea13d0f67d1efcd08603f392dc
mcxiaoke <mcxiaoke@gmail.com>
2016-05-27 15:20:30 +0800

    exif viewer, add model ,using tableview

--- osx_learning_demos/ExifViewer/ExifViewer/ImageDetailViewController.swift
+++ osx_learning_demos/ExifViewer/ExifViewer/ImageDetailViewController.swift
@@ -8,21 +8,87 @@
 
 import Cocoa
 
-class ExifInfo {
+let categoriesTable:[String:String] = [
+  kCGImagePropertyExifDictionary as String:"Exif",
+  kCGImagePropertyExifAuxDictionary as String:"Exif Aux",
+  kCGImagePropertyGPSDictionary as String:"GPS",
+  kCGImagePropertyTIFFDictionary as String:"TIFF",
+  kCGImagePropertyIPTCDictionary as String:"IPTC",
+  kCGImagePropertyJFIFDictionary as String:"JFTF",
+  kCGImagePropertyRawDictionary as String:"Raw",
+  kCGImagePropertyDNGDictionary  as String:"DNG",
+  kCGImagePropertyPNGDictionary as String:"PNG",
+  kCGImagePropertyGIFDictionary  as String:"GIF",
+  kCGImagePropertyMakerAppleDictionary  as String:"Apple",
+  kCGImagePropertyMakerNikonDictionary  as String:"Nikon",
+  kCGImagePropertyMakerCanonDictionary  as String:"Canon",
+  kCGImagePropertyMakerFujiDictionary  as String:"Fuji",
+  kCGImagePropertyMakerOlympusDictionary  as String:"Olympus",
+  kCGImagePropertyMakerPentaxDictionary as String: "Pentax"
+]
+let imageIOBundle = NSBundle(identifier:"com.apple.ImageIO.framework")
+
+class PropertyItem:NSObject {
   let key:String
-  let value:AnyObject?
+  let value:String
+  let cat:String?
+  let rawKey:String
+  let rawValue:AnyObject
+  var rawCat:String?
   
-  init(key:String, value:AnyObject?){
-    self.key = key
-    self.value = value
+  init(rawKey:String, rawValue:AnyObject, rawCat:String?){
+    self.rawKey = rawKey
+    self.rawValue = rawValue
+    self.rawCat = rawCat
+    self.key = PropertyItem.normalizeKey(rawKey, rawCat: rawCat)
+    self.value = PropertyItem.normalizeValue(rawValue)
+    if let rawCat = rawCat {
+      self.cat = PropertyItem.getImageIOLocalizedString(rawCat)
+    }else{
+      self.cat = nil
+    }
+    super.init()
+  }
+  
+  class func normalizeKey(rawKey: String, rawCat:String?) -> String {
+    let key = getImageIOLocalizedString(rawKey)
+    if let prefix = getCategoryPrefix(rawCat) {
+      return "\(prefix) \(key)"
+    }else {
+      return key
+    }
+  }
+  
+  class func normalizeValue(value:AnyObject) -> String {
+    let valueStr:String
+    if let value = value as? NSArray {
+      valueStr = value.componentsJoinedByString(", ")
+    }else {
+      valueStr = "\(value)"
+    }
+    return valueStr
+  }
+  
+  class func getImageIOLocalizedString(key: String) -> String
+  {
+    return imageIOBundle?.localizedStringForKey(key, value: key, table: "CGImageSource") ?? key
+  }
+  
+  class func getCategoryPrefix(category: String?) -> String? {
+    if let category = category {
+      if let prefix = categoriesTable[category]{
+        return "\(prefix) "
+      }
+    }
+    return nil
   }
 }
 
-class ImageInfo {
+class ImageInfo:NSObject {
   let fileName:String
   let width:Int = 0
   let height:Int = 0
-  var properties:[[String:ExifInfo]] = []
+  var properties:[[String:PropertyItem]] = []
   
   init(fileName: String){
     self.fileName = fileName
@@ -39,37 +105,38 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
         loadImageProperties(url)
       }else{
         self.image = nil
-        self.treeController.content = nil
+        self.arrayController.content = nil
       }
     }
   }
   
   dynamic var image:NSImage?
-  dynamic var properties: [NSMutableDictionary]?
+  dynamic var properties: [PropertyItem] = []
   
   @IBOutlet weak var filePathLabel: NSTextField!
   @IBOutlet weak var fileSizeLabel: NSTextField!
   @IBOutlet weak var fileTypeLabel: NSTextField!
   @IBOutlet weak var filePixelLabel:NSTextField!
-  @IBOutlet weak var outlineView: NSOutlineView!
-  @IBOutlet weak var treeController: NSTreeController!
+  @IBOutlet weak var tableView: NSTableView!
+  @IBOutlet weak var arrayController: NSArrayController!
   
   let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
   
   @IBAction func textValueChanged(sender: NSTextField) {
     print("textValueChanged: \(sender.stringValue)")
-    if let object = self.treeController.selectedObjects.first {
-      let row  = self.outlineView.selectedRow
+    if let object = self.arrayController.selectedObjects.first {
+      let row  = self.tableView.selectedRow
       print("outlineViewSelectionDidChange selectedRow = \(row)")
       print("outlineViewSelectionDidChange selectedObject = \(object)")
     }
   }
   
-  func outlineView(outlineView: NSOutlineView, shouldEditTableColumn tableColumn: NSTableColumn?, item: AnyObject) -> Bool {
-    return  false
-  }
-  
   func outlineViewSelectionDidChange(notification: NSNotification) {
+    if let object = self.arrayController.selectedObjects.first {
+      let row  = self.tableView.selectedRow
+      print("outlineViewSelectionDidChange selectedRow = \(row)")
+      print("outlineViewSelectionDidChange selectedObject = \(object)")
+    }
   }
   
   
@@ -102,64 +169,17 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
     }
   }
   
-  func normalizeValue(value:AnyObject) -> String {
-    let valueStr:String
-    if let value2 = value as? NSArray {
-      valueStr = value2.componentsJoinedByString(", ")
-    }else {
-      valueStr = "\(value)"
-    }
-    return valueStr
-  }
-  
-  func ImageIOLocalizedString(key: String) -> String
-  {
-    let bundle = NSBundle(identifier:"com.apple.ImageIO.framework")
-    return bundle?.localizedStringForKey(key, value: key, table: "CGImageSource") ?? key
-  }
-  
-  func parseToTree(properties: NSDictionary) -> [NSMutableDictionary]{
-    let keys = properties.allKeys.sort { (left, right) -> Bool in
-      return (left as! String) < (right as! String)
-    }
-    var array:[NSMutableDictionary] = []
-    for i in 1..<keys.count {
-      let key = keys[i] as! String
-      let locKey = ImageIOLocalizedString(key)
-      let obj = properties.objectForKey(key)!
-      let child: NSMutableDictionary
-      if let obj = obj as? NSDictionary {
-        child =  ["key": locKey, "key2": key, "value":"", "children": self.parseToTree(obj)]
-      }else{
-        child =  ["key": locKey, "key2": key, "value": normalizeValue(obj)]
+  func parseProperties(properties: Dictionary<String,AnyObject>, category:String? = nil) -> [PropertyItem]{
+    var items:[PropertyItem] = []
+    properties.forEach { (key, value) in
+      if let child  = value as? Dictionary<String,AnyObject> {
+        items += parseProperties(child, category: key)
+      }else {
+        let newItem = PropertyItem(rawKey: key, rawValue: value, rawCat: category)
+        items.append(newItem)
       }
-      array.append(child)
     }
-    return array
-  }
-  
-  func parseProperties(properties: NSDictionary) -> [NSMutableDictionary] {
-    var dict = parseToTree(properties)
-    let dummyChild: NSMutableDictionary = ["key": "No Entry Found", "key2": "NoEntryFound", "value": ""]
-    if properties[kCGImagePropertyExifDictionary as String] == nil {
-      let key = kCGImagePropertyExifDictionary as String
-      let locKey = ImageIOLocalizedString(key)
-      let newChild:NSMutableDictionary  = ["key": locKey, "key2": key, "value":"", "children": [dummyChild]]
-      dict.append(newChild)
-    }
-    if properties[kCGImagePropertyGPSDictionary as String] == nil {
-      let key = kCGImagePropertyGPSDictionary as String
-      let locKey = ImageIOLocalizedString(key)
-      let newChild:NSMutableDictionary  = ["key": locKey, "key2": key, "value":"", "children": [dummyChild]]
-      dict.append(newChild)
-    }
-    if properties[kCGImagePropertyTIFFDictionary as String] == nil {
-      let key = kCGImagePropertyTIFFDictionary as String
-      let locKey = ImageIOLocalizedString(key)
-      let newChild:NSMutableDictionary  = ["key": locKey, "key2": key, "value":"", "children": [dummyChild]]
-      dict.append(newChild)
-    }
-    return dict
+    return items
   }
   
   func loadImageProperties(url: NSURL){
@@ -168,12 +188,13 @@ class ImageDetailViewController: NSViewController, NSOutlineViewDelegate {
         if let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as Dictionary?{
           let width = imageProperties[kCGImagePropertyPixelWidth] as! Int
           let height = imageProperties[kCGImagePropertyPixelHeight] as! Int
-          let properties = self.parseProperties(imageProperties)
-          dispatch_async(dispatch_get_main_queue(), {
-            self.filePixelLabel.stringValue = "\(width)X\(height)"
-            self.properties = properties
-            self.outlineView.expandItem(nil, expandChildren: true)
-          })
+          if let imageProperties  = imageProperties as? Dictionary<String,AnyObject> {
+            let properties = self.parseProperties(imageProperties).sort { $0.key < $1.key }
+              dispatch_async(dispatch_get_main_queue(), {
+                self.filePixelLabel.stringValue = "\(width)X\(height)"
+                self.properties = properties
+              })
+          }
         }
       }
     }

f07cb08d464a9dd10a6f2d507275630dcb0a7d7a
mcxiaoke <mcxiaoke@gmail.com>
2016-06-08 10:50:32 +0800

    geo photos: add layout and data

--- osx_learning_demos/MyPlayground.playground/Contents.swift
+++ osx_learning_demos/MyPlayground.playground/Contents.swift
@@ -4,19 +4,19 @@ import Cocoa
 
 var str = "Hello, playground"
 
-let url = NSURL(string:"file://Users/mcxiaoke/Downloads/IOS.2016003043.jpg")!
+let url = NSURL(string:"file:///Users/mcxiaoke/Pictures/20160508_054400164_iOS.jpg")!
 
-url.URLByDeletingPathExtension
+print(url.path)
 
-url.URLByDeletingLastPathComponent
+if let imageSource = CGImageSourceCreateWithURL(url, nil) {
+  print(imageSource)
+  let value = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)!
+  if let properties = value as? [NSObject:AnyObject] {
+    properties.keys.map({ (key) -> String in
+      "\(key)=\(properties[key])\n"
+    }).joinWithSeparator(",")
+  }
 
-url.URLByDeletingPathExtension?.lastPathComponent
-
-url.lastPathComponent
-
-url.path
-
-url.pathExtension
-
-url.pathComponents
-
+}else {
+  print("no source")
+}

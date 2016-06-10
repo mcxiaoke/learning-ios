545d45e7d0e355954dffcbb0abddf52c7bcbbd56
mcxiaoke <mcxiaoke@gmail.com>
2016-06-10 14:50:49 +0800

    geo photos: minor tweak image save

---
+++ osx_learning_demos/MapViewDemo/MapViewDemo/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  MapViewDemo
+//
+//  Created by mcxiaoke on 16/6/10.
+//  Copyright © 2016年 mcxiaoke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+  @IBAction func text1(sender: AnyObject) {
+  }
+
+  @IBAction func text2(sender: AnyObject) {
+  }
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+  
+
+
+}
+
